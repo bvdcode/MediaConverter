4747bb4c17cba4758e877896ac7dd61158dd607d
bvd <BelovVD@tmk-group.com>
2023-01-13 00:29:27 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
