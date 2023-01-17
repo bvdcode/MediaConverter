d3922706603d8bf224d480f4d56093881632c37e
Vadim Belov <BelovVD@tmk-group.com>
2023-01-17 20:27:31 +0600

    Extract Options class to file

---
+++ Sources/MediaConverter.Console/Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+
+namespace MediaConverter.ConsoleClient
+{
+    internal partial class Program
+    {
+        public class Options
+        {
+            [Option('c', "count", Required = false, HelpText = "Calculate input files count.")]
+            public bool CalculateCount { get; set; }
+
+            [Option('i', "input", Required = false, HelpText = "Input directory path, if not specified - using current directory.")]
+            public string? InputDirectory { get; set; }
+
+            [Option('f', "format", Required = true, HelpText = "Calculate input files count.")]
+            public string? OutputFormat { get; set; }
+
+            [Option('r', "reset", Required = false, HelpText = "Flush compressed file hashes from cache.")]
+            public bool ResetCache { get; set; }
+
+            [Option('m', "mark-bad-as-completed", Required = false, HelpText = "Mark bad files as completed.")]
+            public bool MarkBadAsCompleted { get; set; }
+
+            [Option('s', "scan-only", Required = false, HelpText = "Scan only (no convert).")]
+            public bool ScanOnly { get; set; }
+
+
+            [Option('l', "limit", Required = false, HelpText = "Limit files for converting.")]
+            public int Limit { get; set; }
+        }
+    }
+}
--- Sources/MediaConverter.Console/Program.cs
+++ Sources/MediaConverter.Console/Program.cs
@@ -3,7 +3,7 @@ using MediaConverter.Core;
 
 namespace MediaConverter.ConsoleClient
 {
-    internal class Program
+    internal partial class Program
     {
         static async Task Main(params string[] args)
         {
@@ -35,31 +35,6 @@ namespace MediaConverter.ConsoleClient
                 Console.CancelKeyPress += (sender, args) => { cancellationTokenSource.Cancel(); Thread.Sleep(3000); };
                 await cc.ConvertFilesAsync(options.Limit, cancellationTokenSource.Token);
             }
-        }
-
-        public class Options
-        {
-            [Option('c', "count", Required = false, HelpText = "Calculate input files count.")]
-            public bool CalculateCount { get; set; }
-
-            [Option('i', "input", Required = false, HelpText = "Input directory path, if not specified - using current directory.")]
-            public string? InputDirectory { get; set; }
-
-            [Option('f', "format", Required = true, HelpText = "Calculate input files count.")]
-            public string? OutputFormat { get; set; }
-
-            [Option('r', "reset", Required = false, HelpText = "Flush compressed file hashes from cache.")]
-            public bool ResetCache { get; set; }
-
-            [Option('m', "mark-bad-as-completed", Required = false, HelpText = "Mark bad files as completed.")]
-            public bool MarkBadAsCompleted { get; set; }
-
-            [Option('s', "scan-only", Required = false, HelpText = "Scan only (no convert).")]
-            public bool ScanOnly { get; set; }
-
-
-            [Option('l', "limit", Required = false, HelpText = "Limit files for converting.")]
-            public int Limit { get; set; }
         }
     }
 }
