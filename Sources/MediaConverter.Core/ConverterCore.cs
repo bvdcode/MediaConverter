c894590869d253f66977d30b4a45a19baddc7392
BigMak <mail@example.com>
2024-07-02 16:47:16 -0700

    refactor: Improve file deletion logic in FileHelpers.cs

--- Sources/MediaConverter.Core/ConverterCore.cs
+++ Sources/MediaConverter.Core/ConverterCore.cs
@@ -68,6 +68,7 @@ namespace MediaConverter.Core
                 throw new DirectoryNotFoundException(inputDirectory);
             }
             CheckLibraries();
+            _logger = logger;
             _outputFormat = outputFormat.Replace(".", string.Empty).Trim();
             _inputCache = new List<FileInfo>();
             _inputFormats = DetectInputFormats();
@@ -94,6 +95,7 @@ namespace MediaConverter.Core
             {
                 return StreamType.Audio;
             }
+            errorCounter++;
             throw new NotSupportedException("Output media type is not supported: " + _outputFormat);
         }
 
@@ -127,6 +129,7 @@ namespace MediaConverter.Core
                     _ => throw new NotSupportedException("Output media type is not supported: " + _outputFormat)
                 };
             }
+            errorCounter++;
             throw new NotSupportedException("Output media type is not supported: " + _outputFormat);
         }
 
@@ -146,7 +149,7 @@ namespace MediaConverter.Core
         public async Task FindInputFilesAsync()
         {
             await Task.Run(() => _inputCache.AddRange(GetInputFilesLazy()));
-            Log("Found supported input files: {0}", _inputCache.Count);
+            _logger.Information("Found supported input files: {0}", _inputCache.Count);
         }
 
         public void ResetCache()
@@ -155,7 +158,7 @@ namespace MediaConverter.Core
             if (Directory.Exists(folder))
             {
                 Directory.Delete(folder, true);
-                Log("Cache folder was deleted.");
+                _logger.Information("Cache folder was deleted.");
             }
             Directory.CreateDirectory(folder);
         }
@@ -163,7 +166,7 @@ namespace MediaConverter.Core
         private IEnumerable<FileInfo> GetInputFilesLazy()
         {
             var allFiles = FileHelpers.GetFiles(_inputDirectory, _inputFormats);
-            Log("Search for input files from {0} files count...", allFiles.Count());
+            _logger.Information("Search for input files from {0} files count...", allFiles.Count());
             foreach (var file in allFiles)
             {
                 if (!IsConverted(file))
@@ -175,7 +178,7 @@ namespace MediaConverter.Core
                     skipCounter++;
                     if (skipCounter % 100 == 0)
                     {
-                        Log("Skipped {0} files", skipCounter);
+                        _logger.Information("Skipped {0} files", skipCounter);
                     }
                 }
             }
@@ -266,7 +269,7 @@ namespace MediaConverter.Core
             }
             catch (Exception ex)
             {
-                Log(ex, "Bad file - " + file.Name);
+                _logger.Error(ex, "Error when checking codec of file: {0}", file.Name);
                 if (_markBadAsCompleted)
                 {
                     SetAsConvertedByMetadata(file);
@@ -321,14 +324,14 @@ namespace MediaConverter.Core
         {
             DeleteTempFiles();
             string currentDirectory = string.Empty;
-            Log("Supported input media types: {0}", string.Join(", ", _inputFormats));
+            _logger.Information("Output format: {0}", _outputFormat);
             var inputFiles = _inputCache.Count > 0 ? _inputCache : GetInputFilesLazy();
             foreach (var inputFile in inputFiles)
             {
                 if (currentDirectory != inputFile.DirectoryName)
                 {
                     currentDirectory = inputFile.DirectoryName;
-                    Log("Current directory: {0}", currentDirectory.Replace(_inputDirectory.FullName, string.Empty));
+                    _logger.Information("Current directory: {0}", currentDirectory.Replace(_inputDirectory.FullName, string.Empty));
                 }
                 try
                 {
@@ -336,7 +339,8 @@ namespace MediaConverter.Core
                 }
                 catch (Exception ex)
                 {
-                    Log(ex, "Error when file converting");
+                    errorCounter++;
+                    _logger.Error(ex, "Error when file converting");
                 }
                 if (limit > 0)
                 {
@@ -357,20 +361,20 @@ namespace MediaConverter.Core
         {
             Stopwatch sw = Stopwatch.StartNew();
             string counter = _inputCache.Count > 0 ? $" ({processedCounter + 1}/{_inputCache.Count})" : string.Empty;
-            Log("File: {0}{1}", inputFile.Name, counter);
+            _logger.Information("Processing file: {0}{1}", inputFile.Name, counter);
             FileInfo temp = FileHelpers.GetTempFile(_outputFormat, applicationName);
-            var snippet = await Xabe.FFmpeg.FFmpeg.Conversions.FromSnippet.Convert(inputFile.FullName, temp.FullName);
+            var snippet = await FFmpeg.Conversions.FromSnippet.Convert(inputFile.FullName, temp.FullName);
             snippet.OnProgress += Snippet_OnProgress;
             snippet.AddParameter($"-metadata {applicationName}={true}");
             if (_ignoreErrors)
             {
-                snippet.AddParameter("-err_detect ignore_err", Xabe.FFmpeg.ParameterPosition.PreInput);
+                snippet.AddParameter("-err_detect ignore_err", ParameterPosition.PreInput);
             }
             if (_copyCodec)
             {
-                snippet.AddParameter("-c copy", Xabe.FFmpeg.ParameterPosition.PostInput);
+                snippet.AddParameter("-c copy", ParameterPosition.PostInput);
             }
-            Xabe.FFmpeg.IConversionResult result = await snippet.Start(token);
+            IConversionResult result = await snippet.Start(token);
             if (token.IsCancellationRequested)
             {
                 return;
@@ -394,31 +398,12 @@ namespace MediaConverter.Core
 
         #endregion
 
-        #region Logging
-
-        private void Log(Exception exception, string caption)
-        {
-            errorCounter++;
-            LogOutput?.Invoke(this, string.Format("[ERROR] {0} - {1} ({2})", DateTime.Now.ToString("dd MMM HH:mm:ss"), caption, exception.ToString()));
-        }
-
-        private void Log(string message)
-        {
-            LogOutput?.Invoke(this, string.Format("[INFO] {0} - {1}", DateTime.Now.ToString("dd MMM HH:mm:ss"), message));
-        }
-
-        private void Log(string message, params object[] args)
-        {
-            LogOutput?.Invoke(this, string.Format("[INFO] {0} - {1}", DateTime.Now.ToString("dd MMM HH:mm:ss"), string.Format(message, args)));
-        }
-
-        #endregion
-
         #region Hooks
 
         private void OnWorkCompleted()
         {
-            Log("Done. Processed {0} files. Compressed {1} MBytes. Errors: {2}. Elapsed: {3}", processedCounter, compressedBytes / 1024 / 1024, errorCounter, _totalElapsed);
+            _logger.Information("Done. Processed {0} files. Compressed {1} MBytes. Errors: {2}. Elapsed: {3}",
+                processedCounter, compressedBytes / 1024 / 1024, errorCounter, _totalElapsed);
         }
 
         private void OnItemProcessed(FileInfo inputFile, TimeSpan elapsed, long newSize, long oldSize)
@@ -430,7 +415,7 @@ namespace MediaConverter.Core
             long oldSizeMb = oldSize / 1024 / 1024;
             long newSizeMb = newSize / 1024 / 1024;
             int compressionRate = (int)(compressed * 100 / oldSize);
-            Log("Compressed file: {0}, {1}Mb => {2}Mb ({3}%), elapsed: {4}", inputFile.Name, oldSizeMb, newSizeMb, compressionRate, elapsed);
+            _logger.Information("Compressed file: {0}, {1}Mb => {2}Mb ({3}%), elapsed: {4}", inputFile.Name, oldSizeMb, newSizeMb, compressionRate, elapsed);
             SetAsConvertedByMetadata(inputFile);
         }
 
@@ -441,7 +426,7 @@ namespace MediaConverter.Core
                 return;
             }
             progressCounter = args.Percent;
-            Log("Progress: {0}% ({1} - {2}) PID: {3}", args.Percent, args.Duration, args.TotalLength, args.ProcessId);
+            _logger.Information("Progress: {0}% ({1} - {2}) PID: {3}", args.Percent, args.Duration, args.TotalLength, args.ProcessId);
         }
 
         #endregion
